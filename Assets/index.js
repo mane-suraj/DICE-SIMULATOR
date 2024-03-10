2292e0b90790ec32a505064cc43a32db03c3681a
Suraj Mane <68557968+mane-suraj@users.noreply.github.com>
2024-03-10 15:44:32 +0530

    Add files via upload

---
+++ Assets/index.js
@@ -0,0 +1,46 @@
+var userName = window.prompt("Enter you Name");
+if (userName != "")
+{
+    document.querySelectorAll(".playerContainer h2")[0].innerHTML = userName;
+}
+else
+{
+    userName = "Player 1"
+    document.querySelectorAll(".playerContainer h2")[0].innerHTML = userName;
+}
+
+const btn = document.querySelector("#rollBtn");
+
+btn.onclick = function()
+{
+
+    var randomNumber1 = Math.floor((Math.random())*6)+1;
+
+    var randomImage1 = "Dice"+randomNumber1+".jpg";
+    var path1 = "./Images/"+randomImage1;
+    var changeImage1 = document.querySelectorAll("img")[0];
+    changeImage1.setAttribute("src", path1)
+
+    var randomNumber2 = Math.floor((Math.random())*6)+1;
+    var randomImage2 = "Dice"+randomNumber2+".jpg";
+    var path2 = "./Images/"+randomImage2;
+    var changeImage2 = document.querySelectorAll("img")[1];
+    changeImage2.setAttribute("src", path2)
+
+
+    var result = document.querySelector("#Result");
+    result.classList.add("afterRollResult");
+
+    if(randomNumber1>randomNumber2)
+    {
+        result.innerHTML = userName + " Wins!! &#x1F389;";
+    }
+    else if (randomNumber1<randomNumber2)
+    {
+        result.innerHTML = "Computer Wins!! &#x1F389;";
+    }
+    else
+    {
+        result.innerHTML = "Match Draw!";
+    }
+}
